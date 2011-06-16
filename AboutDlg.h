04489f981c6767fe26022e9c8ed430bc8dc5e62f
Unix9N@gmail.com <Unix9N@gmail.com@8bde135f-3fd0-328e-5674-3dabc69dabfb>
2011-06-16 18:22:14 +0000

    SA Release 1.0.0.1 (Added Source Files)

---
+++ AboutDlg.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+// CAboutDlg dialog
+
+class CAboutDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CAboutDlg)
+
+public:
+	CAboutDlg(CWnd* pParent = NULL);   // standard constructor
+	virtual ~CAboutDlg();
+
+// Dialog Data
+	enum { IDD = IDD_ABOUT_DLG };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnNMClickProjLink(NMHDR *pNMHDR, LRESULT *pResult);
+};
